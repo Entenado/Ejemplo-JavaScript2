020d375ead2d237857c8dbb20977615f4ecb91a3
Entenado <110743062+Entenado@users.noreply.github.com>
2022-08-18 17:33:32 -0300

    Subo primer entrega Coder
    Pruebo desde un archivo J.S ya creado, extrayendo a Github para ver si lo toma

---
+++ miarchivo.js
@@ -0,0 +1,20 @@
+/* En este apartado trataremos de ayudar a la profesora Miriam sacando un promedio del alumno Juan,
+determinando si la suma de los 3 trimestres permite a Juan pasar de año o repetir el mismo.
+La nota para aprobar los trimestres es 7*/
+let trimestre1 = parseFloat(prompt("ingrese nota1"));
+let trimestre2 = parseFloat(prompt("ingrese nota2"));
+let trimestre3 = parseFloat(prompt ("ingrese nota3"));
+
+let promedio = trimestre1 + trimestre2 + trimestre3;
+
+let totalNota = parseInt (promedio / 3);
+if (totalNota >= 7 && totalNota <= 10 ){
+      console.log(totalNota)
+}
+do {totalNota = false
+}while (totalNota >= 7){
+   console.log("Juan pasa de año")
+}
+if (totalNota<=7){
+   console.log("Juan no pasa de año")
+}
