426d6b9a20b9ef5c7e6217235242e5491690f3bd
Entenado <fuciliabel@gmail.com>
2022-09-20 18:04:43 -0300

    Entrega nro 5
    Se determina la desestructuracion y el operador ternario or para definir "deposito", aún se trabaja en la vinculacion con html

--- miarchivo2.js
+++ miarchivo2.js
@@ -54,16 +54,19 @@ alert("No hay nommbres en el local storage");
 
    function guardar(){
         let usuario1 ={
-          usuario : "goribe",
-          contraseña : "function123",
+          usuario1 : "goribe",
+          contraseña1 : "function123",
+          deposito1 : 450000,
         };
         let usuario2 ={
-          usuario : "proque",
-          contraseña : "solar1",
+          usuario2 : "proque",
+          contraseña2 : "solar1",
+          deposito2 : 750000,
         };
         let usuario3 ={
-          usuario : "ssalvo",
-          contraseña : "parque43",
+          usuario3 : "ssalvo",
+          contraseña3 : "parque43",
+          deposito3 : 800000,
         };
 
         localStorage.setItem("usuario1", JSON.stringify(usuario1));
@@ -71,26 +74,31 @@ alert("No hay nommbres en el local storage");
         localStorage.setItem("usuario3", JSON.stringify(usuario3));
       }
 
-
+   
      
 //captura();
       function captura(){
-        var nombreus=document.getElementById("txtuser").value;
-        var contraus=document.getElementById("txtpassword").value;
+        
+        let nombreus=document.getElementById("txtuser").value;
+        let contraus=document.getElementById("txtpassword").value;
         if (nombreus=="goribe" && contraus=="function123"){
           alert("Bienvenido Gabriel Oribe");
-          console.log (nombreus +" "+  contraus);
+          location.href = "seleccionar.html"
+          console.log (user1);
+          
           
           
         }
         else if (nombreus=="proque" && contraus=="solar1"){
           alert("Bienvenido Pedro Roque");
-          console.log (nombreus +" "+ contraus);
+          location.href = "seleccionar.html"
+          console.log (user2);
           
         }
         else if (nombreus=="ssalvo" && contraus=="parque43"){
           alert("Bienvenida Susana Salvo");
-          console.log (nombreus +" "+ contraus);
+          location.href = "seleccionar.html"
+          console.log (user3);
           
         }
           else if(nombreus==""){
@@ -99,85 +107,16 @@ alert("No hay nommbres en el local storage");
           }else if(contraus==""){
             alert("Debe ingresar un password")
             document.getElementById("txtpassword").focus();
-  
+          
 
         }else{
         alert("error");
-        document.getElementById("txtpassword").value;
         
       }
       }
 
 
 
-
-//dejo comentado la segunda parte, ya que aun no hay modificaciones, al no saber como avanzar de pagina
-//cuando se  valida el ingreso. A espera de la respuesta del tutor
-
-
-/*
-// En esta primer parte solo determinamos por consola, quien de los primeros 3 clientes de nuestro banco virtual entrará
-let Seleccionar = prompt ("Ingrese opcion (1) si desea hacer un retiro ó ingrese opcion (2) si desea constituir un plazo fijo");
-  //Por ahora solo tenemos 2 opciones ya sea extracción o constitución de plazo fijo
-  const extraccion = {
-  fondos : parseInt(prompt("Ingrese Fondos")),
-  
-  montoExtraccion : parseInt (prompt("Ingrese extraccion")),
-  
-  saldo : ""}
-  
-  function cajero (fondos, montoExtraccion) {
-  
-  extraccion.saldo = extraccion.fondos - extraccion.montoExtraccion
-  
-  return extraccion.saldo}
-  
-  
-  cajero(extraccion.fondos, extraccion.montoExtraccion);
-  
-  
-  let deposito = parseFloat(prompt("Ingrese monto a depositar"));
-  let plazos = parseFloat[30,60,90];
-  plazos = parseInt(prompt("Ingrese plazo"));
-
- function plazoFijo(deposito){
-    
-      if (plazos == 30) {
-        deposito = (deposito * 0.20)+ deposito;
-        return deposito;
-      }
-      else if (plazos == 60){
-        deposito = (deposito * 0.40) + deposito;
-        return deposito;
-      }
-      else if (plazos == 90){
-        deposito = (deposito * 0.60)+ deposito;
-        return deposito;
-      }
-      else { 
-        alert("error");
-      }
-      
-    };
-  
-  // En este apartado seleccionamos la opcion a realizar.
-  if (Seleccionar ==1){
-  console.log("Usted posee en la cuenta la suma de", extraccion.fondos, "pesos disponibles.");
-  
-  console.log("Usted solicitó extraer", extraccion.montoExtraccion, "pesos.");
-  
-  console.log("El saldo disponible es de", extraccion.saldo, "pesos.");
-  }
-  else if (Seleccionar == 2){
-    console.log ("usted constituyó un plazo fijo a", plazos, "dias");
-    console.log ("una vez finalizado el periodo, usted obtendrá la suma de",plazoFijo(deposito),"pesos");
-  }
-  else {
-    alert ("error")
-  };
-  /* Si bien está desordenado por falta de tiempo,
-   hice la primer parte captuurando datos de formulario utilizando la funcion "Capturar"
-   Falta la siguiente parte.*/
 
 
 
---
+++ seleccionar.js
@@ -0,0 +1,67 @@
+// En esta primer parte solo determinamos por consola, quien de los primeros 3 clientes de nuestro banco virtual entrará
+//let Seleccionar = prompt ("Ingrese opcion (1) si desea hacer un retiro ó ingrese opcion (2) si desea constituir un plazo fijo");
+  //Por ahora solo tenemos 2 opciones ya sea extracción o constitución de plazo fijo
+  
+let user1 = JSON.parse(localStorage.getItem("usuario1"))
+let user2 = JSON.parse(localStorage.getItem("usuario2"))
+let user3 = JSON.parse(localStorage.getItem("usuario3"))
+
+let{usuario1, contraseña1, deposito1} = user1;
+console.log(deposito1)
+let{usuario2, contraseña2, deposito2} = user2;
+console.log(deposito2)
+let{usuario3, contraseña3, deposito3} = user3;
+console.log(deposito3)
+
+let deposito = [deposito1 || deposito2 || deposito3]
+
+let extraccion = document.getElementById("extraccion").value;
+
+let extraer = deposito - extraccion;
+
+console.log(extraer)
+
+
+
+  /*let deposito = parseFloat(prompt("Ingrese monto a depositar"));
+  let plazos = parseFloat[30,60,90];
+  plazos = parseInt(prompt("Ingrese plazo"));
+
+ function plazoFijo(deposito){
+    
+      if (plazos == 30) {
+        deposito = (deposito * 0.20)+ deposito;
+        return deposito;
+      }
+      else if (plazos == 60){
+        deposito = (deposito * 0.40) + deposito;
+        return deposito;
+      }
+      else if (plazos == 90){
+        deposito = (deposito * 0.60)+ deposito;
+        return deposito;
+      }
+      else { 
+        alert("error");
+      }
+      
+    };
+  
+  // En este apartado seleccionamos la opcion a realizar.
+  if (Seleccionar ==1){
+  console.log("Usted posee en la cuenta la suma de", extraccion.fondos, "pesos disponibles.");
+  
+  console.log("Usted solicitó extraer", extraccion.montoExtraccion, "pesos.");
+  
+  console.log("El saldo disponible es de", extraccion.saldo, "pesos.");
+  }
+  else if (Seleccionar == 2){
+    console.log ("usted constituyó un plazo fijo a", plazos, "dias");
+    console.log ("una vez finalizado el periodo, usted obtendrá la suma de",plazoFijo(deposito),"pesos");
+  }
+  else {
+    alert ("error")
+  };
+  /* Si bien está desordenado por falta de tiempo,
+   hice la primer parte captuurando datos de formulario utilizando la funcion "Capturar"
+   Falta la siguiente parte.*/
